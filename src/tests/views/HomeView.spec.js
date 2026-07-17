6baf0d974e6a3c60bdcc8d027f38401e45492f38
Ekaterina Zotova <139338863+zookatt@users.noreply.github.com>
2026-07-17 09:39:26 +0200

    test: mock homeview from external api calls

--- src/tests/views/HomeView.spec.js
+++ src/tests/views/HomeView.spec.js
@@ -1,8 +1,19 @@
 import { mount } from "@vue/test-utils";
 import { createPinia } from "pinia";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 
 import HomeView from "../../views/HomeView.vue";
+
+vi.mock(
+  "../../core/apis/currency-freaks/currencyService",
+  () => ({
+    getCurrencyRates: vi.fn().mockResolvedValue({
+      EUR: 0.8,
+      USD: 1,
+      JPY: 150,
+    }),
+  }),
+);
 
 describe("HomeView", () => {
   function createWrapper() {
